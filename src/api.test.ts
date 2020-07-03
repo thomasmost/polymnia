5cf38fa1a4109edb7a828d7b9e1b8a25137e82d9
thomas/NERO <tomconmo@gmail.com>
2020-07-03 17:04:20 -0400

    1.0.2

--- src/api.test.ts
+++ src/api.test.ts
@@ -5,52 +5,51 @@ jest.mock('./make_request');
 
 test('suggestions api', () => {
   suggest('boo');
-  expect((makeRequest as any).mock.calls[0][0]).toBe("/sug?s=boo");
+  expect((makeRequest as any).mock.calls[0][0]).toBe('/sug?s=boo');
 });
-
 
 describe('words api', () => {
   describe('startsWith, endsWith, and wildCount', () => {
     test('startsWith', () => {
       words({
-        startsWith: 'j'
+        startsWith: 'j',
       });
-      expect((makeRequest as any).mock.calls[0][0]).toBe("/words?sp=j*");
-    })
+      expect((makeRequest as any).mock.calls[0][0]).toBe('/words?sp=j*');
+    });
     test('endsWith', () => {
       words({
-        endsWith: 'k'
+        endsWith: 'k',
       });
-      expect((makeRequest as any).mock.calls[0][0]).toBe("/words?sp=*k");
-    })
+      expect((makeRequest as any).mock.calls[0][0]).toBe('/words?sp=*k');
+    });
     test('starts and ends with', () => {
       words({
         startsWith: 'j',
-        endsWith: 'k'
+        endsWith: 'k',
       });
-      expect((makeRequest as any).mock.calls[0][0]).toBe("/words?sp=j*k");
-    })
+      expect((makeRequest as any).mock.calls[0][0]).toBe('/words?sp=j*k');
+    });
     test('startsWith and fixed characters', () => {
       words({
         startsWith: 'j',
-        wildCount: 5
+        wildCount: 5,
       });
-      expect((makeRequest as any).mock.calls[0][0]).toBe("/words?sp=j?????");
-    })
+      expect((makeRequest as any).mock.calls[0][0]).toBe('/words?sp=j?????');
+    });
     test('endsWith and fixed characters', () => {
       words({
         endsWith: 'k',
-        wildCount: 3
+        wildCount: 3,
       });
-      expect((makeRequest as any).mock.calls[0][0]).toBe("/words?sp=???k");
-    })
+      expect((makeRequest as any).mock.calls[0][0]).toBe('/words?sp=???k');
+    });
     test('starts and ends with and fixed characters', () => {
       words({
         startsWith: 'j',
         endsWith: 'k',
-        wildCount: 2
+        wildCount: 2,
       });
-      expect((makeRequest as any).mock.calls[0][0]).toBe("/words?sp=j??k");
-    })
-  })
+      expect((makeRequest as any).mock.calls[0][0]).toBe('/words?sp=j??k');
+    });
+  });
 });
